d39d1d2b647f9a898bccc7d281a61ebfffdcd328
Pablo Stefan Gomes Pereira <pablo.stefan.dev@gmail.com>
2024-03-22 17:27:24 -0300

    Added flutter triple for state management

--- module_commons/lib/commons.dart
+++ module_commons/lib/commons.dart
@@ -1,3 +1,4 @@
 library module_commons;
 
 export 'package:module_commons/helpers/routes.dart';
+export 'package:module_commons/helpers/colors.dart';
---
+++ module_commons/lib/helpers/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color monoBlack = Colors.black;
+  static const Color monoWhite = Colors.white;
+}
